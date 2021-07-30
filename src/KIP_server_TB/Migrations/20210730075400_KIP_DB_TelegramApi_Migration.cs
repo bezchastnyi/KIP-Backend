4eec96042500de4f41ec14cff04d1c8437f8295d
Alexei Bezchastnyi <abezchastnyi@teamworkcommerce.com>
2021-07-30 10:55:46 +0300

    fix migration

---
+++ src/KIP_server_TB/Migrations/20210730075400_KIP_DB_TelegramApi_Migration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace KIP_server_TB.Migrations
+{
+    /// <inheritdoc />
+    public partial class KIP_DB_TelegramApi_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    UserEmail = table.Column<string>(type: "text", nullable: true),
+                    UserPassword = table.Column<string>(type: "text", nullable: true),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    Faculty = table.Column<string>(type: "text", nullable: true),
+                    Course = table.Column<int>(type: "integer", nullable: true),
+                    Group = table.Column<string>(type: "text", nullable: true),
+                    TempProfValue = table.Column<int>(type: "integer", nullable: true),
+                    TempBuildingValue = table.Column<int>(type: "integer", nullable: true),
+                    TempAudienceValue = table.Column<int>(type: "integer", nullable: true),
+                    TempDayValue = table.Column<int>(type: "integer", nullable: true),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserEmail",
+                table: "Users",
+                column: "UserEmail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserId",
+                table: "Users",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
